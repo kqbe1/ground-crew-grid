075a7e1b92b7537aa41ebc4bcd272e75b130333b
Kevin Quoilin <kevin.quoilin@gmail.com>
2026-08-21 17:35:48 +0200

    chore: migrate Capacitor app away from Lovable

---
+++ android/app/src/main/java/app/lovable/c2f1872aa377467385d4a9b0eb3a58b4/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.c2f1872aa377467385d4a9b0eb3a58b4;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.lovable.c2f1872aa377467385d4a9b0eb3a58b4',
   appName: 'ground-crew-grid',
-  webDir: 'dist',
-  server: {
-    url: "https://c2f1872a-a377-4673-85d4-a9b0eb3a58b4.lovableproject.com?forceHideBadge=true",
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
